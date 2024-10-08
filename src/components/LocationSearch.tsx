0b1c28b5c0566e1ea3c8c9dd05204d9ab83d759d
pjblecha <44296698+pjblecha@users.noreply.github.com>
2024-10-08 08:49:27 -0500

    #451

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,4 +1,13 @@
-export default function LocationSearch() {
+import type { Place } from "../api/Place";
+import { useState } from "react";
+
+interface LocationSearchProps {
+    onPlaceClick: (place: Place) => void;
+}
+
+export default function LocationSearch({ onPlaceClick }) {
+    const [places, setPlaces] = useState<Place[]>([]);
+
     return (
         <div>Search for location</div>
     )
