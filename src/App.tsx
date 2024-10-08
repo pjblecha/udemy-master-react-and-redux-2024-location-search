23c8915599a7043bea3b8d2a1315471f59edae58
pjblecha <44296698+pjblecha@users.noreply.github.com>
2024-10-08 08:30:13 -0500

    #446

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import Map from "./components/Map";
+import LocationSearch from "./components/LocationSearch";
+
 function App() {
   return (
-    <div className="m-4">Getting started on Location Search (A Google Maps clone)</div>
+    <div className="h-screen w-screen grid grid-cols-12">
+      <div className="col-span-3 p-2">
+        <LocationSearch />
+      </div>
+      <div className="col-span-9">
+        <Map />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/LocationSearch.tsx
@@ -0,0 +1,5 @@
+export default function LocationSearch() {
+    return (
+        <div>Search for location</div>
+    )
+}
